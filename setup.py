4ddfedc9b38cabde90190ba49ee46049af24667b
Talley Lambert <talley.lambert@gmail.com>
2020-05-04 15:29:37 -0400

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="minimal-deploy",
+    version='0.0.0',
+    description='A minimal Python package',
+    author='Talley',
+    author_email='talley.lambert@gmail.com',
+    url='https://github.com/tlambert03/minimal-deploy',
+    py_modules=['minimal']
+)
